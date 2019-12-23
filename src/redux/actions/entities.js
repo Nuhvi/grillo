4bbf826ea932f0fba3495ed417fece7c1bd670f5
nazeh <ar.nazeh@gmail.com>
2019-12-23 10:56:59 +0300

    :rotating_light: Small fixes for eslint errors

--- src/redux/actions/entities.js
+++ src/redux/actions/entities.js
@@ -1,9 +1,7 @@
 import { entities } from './types';
 
-export const placeHolder = () => ({
-  type: entities.PLACE_HOLDER,
-});
-
 export const doSomething = () => ({
   type: entities.DO_SOMETHING,
 });
+
+export default { doSomething };
--- src/redux/actions/types/index.js
+++ src/redux/actions/types/index.js
@@ -1,6 +1,5 @@
 export const placeHolder = {};
 
 export const entities = {
-  PLACE_HOLDER: `PLACE_HOLDER`,
   DO_SOMETHING: `DO_SOMETHING`,
 };
