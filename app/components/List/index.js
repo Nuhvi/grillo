2a640296e029ca427b474559c1675ae8e2173cc5
nazeh <ar.nazeh@gmail.com>
2020-01-01 14:53:26 +0300

    :sparkles: Lists are draggable (not persistent)

--- app/components/List/index.js
+++ app/components/List/index.js
@@ -7,25 +7,36 @@
 import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import Cards from 'containers/Cards';
+import { Draggable } from 'react-beautiful-dnd';
 import ListTitle from './ListTitle';
 
 import Wrapper from './ListWrapper';
 
-function List(props) {
-  const { title, id } = props.list;
+function List({ list, draggableIndex }) {
+  const { title, id } = list;
 
   return (
-    <div>
-      <Wrapper component="article" elevation={4}>
-        <ListTitle>{title}</ListTitle>
-        <Cards idList={id} />
-      </Wrapper>
-    </div>
+    <Draggable draggableId={id} index={draggableIndex}>
+      {provided => (
+        <Wrapper
+          component="article"
+          elevation={4}
+          className="list"
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          <ListTitle>{title}</ListTitle>
+          <Cards idList={id} />
+        </Wrapper>
+      )}
+    </Draggable>
   );
 }
 
 List.propTypes = {
   list: PropTypes.object.isRequired,
+  draggableIndex: PropTypes.number,
 };
 
 export default memo(List);
--- app/containers/Lists/index.js
+++ app/containers/Lists/index.js
@@ -9,26 +9,44 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
-
 import { useInjectSaga } from 'utils/injectSaga';
 import { useInjectReducer } from 'utils/injectReducer';
 import Form from 'components/FormAddList';
 import List from 'components/List';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import makeSelectLists from './selectors';
 import reducer from './reducer';
 import saga from './saga';
 import ListsCanvas from './ListsCanvas';
 import { addList } from './actions';
-
 export function Lists({ idBoard, lists, onAddList }) {
   useInjectReducer({ key: 'allLists', reducer });
   useInjectSaga({ key: 'allLists', saga });
 
   return (
     <ListsCanvas>
-      {lists.map(list => (
-        <List key={list.id} list={list} />
-      ))}
+      <DragDropContext onDragEnd={() => {}}>
+        <Droppable
+          droppableId="all-droppables"
+          direction="horizontal"
+          type="list"
+        >
+          {provided => (
+            <div
+              style={{ display: 'flex' }}
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {lists.map((list, index) => (
+                <div key={list.id}>
+                  <List list={list} draggableIndex={index} />
+                </div>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
       <Form idBoard={idBoard} submitHandler={onAddList} />
     </ListsCanvas>
   );
