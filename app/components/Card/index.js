b1fad2518a0fa4c0d35fed5b624bec9e12ca07c8
nazeh <ar.nazeh@gmail.com>
2020-02-05 16:56:21 +0300

    :sparkles: Make Cards draggble

--- app/components/Card/index.js
+++ app/components/Card/index.js
@@ -7,6 +7,7 @@
 import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import { Card, Typography } from '@material-ui/core';
+import { Draggable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
 
 const Wrapper = styled(Card)`
@@ -14,20 +15,31 @@ const Wrapper = styled(Card)`
   padding: 0 ${props => props.theme.spacing(2)}px;
 `;
 
-function CardItem({ card }) {
-  const { title } = card;
+const CardItem = ({ card, index }) => {
+  const { title, id } = card;
 
   return (
-    <Wrapper component="article" variant="outlined">
-      <Typography variant="h6" component="h4">
-        {title}
-      </Typography>
-    </Wrapper>
+    <Draggable draggableId={`${id}`} index={index}>
+      {provided => (
+        <Wrapper
+          component="article"
+          variant="outlined"
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          <Typography variant="h6" component="h4">
+            {title}
+          </Typography>
+        </Wrapper>
+      )}
+    </Draggable>
   );
-}
+};
 
 CardItem.propTypes = {
   card: PropTypes.object.isRequired,
+  index: PropTypes.number.isRequired,
 };
 
 export default memo(CardItem);
