b22af142f7d7b7b92037a8e846f2ae16ebabe35e
nazeh <ar.nazeh@gmail.com>
2020-02-12 22:27:48 +0300

    :construction: Start Project container

---
+++ app/containers/Project/index.js
@@ -0,0 +1,56 @@
+/**
+ *
+ * Project
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Helmet } from 'react-helmet';
+import { FormattedMessage } from 'react-intl';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import { useInjectSaga } from 'utils/injectSaga';
+import { useInjectReducer } from 'utils/injectReducer';
+import makeSelectProject from './selectors';
+import reducer from './reducer';
+import saga from './saga';
+import messages from './messages';
+
+export function Project() {
+  useInjectReducer({ key: 'project', reducer });
+  useInjectSaga({ key: 'project', saga });
+
+  return (
+    <div>
+      <Helmet>
+        <title>Project</title>
+        <meta name="description" content="Description of Project" />
+      </Helmet>
+      <FormattedMessage {...messages.header} />
+    </div>
+  );
+}
+
+Project.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  project: makeSelectProject(),
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+export default compose(withConnect)(Project);
---
+++ app/containers/Project/messages.js
@@ -0,0 +1,16 @@
+/*
+ * Project Messages
+ *
+ * This contains all the text for the Project container.
+ */
+
+import { defineMessages } from 'react-intl';
+
+export const scope = 'app.containers.Project';
+
+export default defineMessages({
+  header: {
+    id: `${scope}.header`,
+    defaultMessage: 'This is the Project container!',
+  },
+});
